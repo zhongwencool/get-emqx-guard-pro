5d28184827a98dfd4c5120c637ebe2c35179d1bd
zhongwencool <zhongwencool@gmail.com>
2024-10-18 15:26:27 +0800

    feat: add deploy github action

--- rspress.config.ts
+++ rspress.config.ts
@@ -4,6 +4,7 @@ import alignImage from 'rspress-plugin-align-image';
 
 export default defineConfig({
   root: path.join(__dirname, 'docs'),
+  //base: '/get-emqx-guard-pro/',
   title: 'EMQX Guard Pro',
   description: 'EMQX-Guard-Pro is EMQX plugin that keeping your EMQX cluster robust and secure, provides real-time notifications to your email, slack, dingtalk, wechat, feishu and more.',
   icon: '/emqx-guardian.svg',
